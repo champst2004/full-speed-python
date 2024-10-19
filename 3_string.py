ad196740b6ab1bd98e68e81422835c31c081d667
champst2004 <st.is.champion@gmail.com>
2024-10-19 17:38:33 +0530

    1st

---
+++ 3_string.py
@@ -0,0 +1,12 @@
+def getStr(s):
+  s=s[:1] + s[0] + s[1:]
+  s=s[:1] + s[0] + s[1:]
+  s=s[:3] + s[3] + s[3:]
+  s=s[:3] + s[3] + s[3:]
+  s=s[:6] + s[6] + s[6:]
+  s=s[:6] + s[6] + s[6:]
+  strlen = len(s)
+  return [s, strlen]
+
+print(getStr("abc"))
+print(getStr("xyz"))
